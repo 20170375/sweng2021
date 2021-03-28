e8ee4adbf5336cb111a25a7e4dc526dee6b68749
20170375 <20170375@naver.com>
2021-03-28 22:11:31 +0900

    final

--- pytet_v0.4/ctetris.py
+++ pytet_v0.4/ctetris.py
@@ -87,8 +87,8 @@ class CTetris((Tetris)):
                 if array[y][x] == 0:
                     break
             else:
-                for line in range(y, -1, -1):
-                    array[line] = array[line-1]
+                for line in range(y, 0, -1):
+                    array[line] = array[line-1][ : ]
                 for x in range(CTetris.iScreenDw, self.oScreen.get_dx()-CTetris.iScreenDw):
                     array[0][x] = 0
                 self.oScreen = Matrix(array)
